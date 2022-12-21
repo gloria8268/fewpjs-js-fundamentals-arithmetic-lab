b767e30287fe61b96ed207a7ab20ff574040e773
gloria <gloria.gao115@gmail.com>
2022-12-21 16:16:19 +0800

    complete

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+const num1 = 2
+const num2 = 31
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random() * 10)
+console.log(Number.isInteger(random))
+
+const num3 = 11
+const num4 = 7
+let mod = num3 % num4
+
+let max = Math.max(1, 4, 6, 4, 20)
+
